eef5e9a54c4b6314137e5d0ac8beae197b86925b
Misjly <nikmisg@gmail.com>
2023-02-15 17:08:56 -0600

    Router Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
@@ -13,7 +13,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
+    <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+    </React.StrictMode>
 );
