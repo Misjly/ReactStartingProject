39952efa6f5c282142d11385863a124cc516234f
Misjly <nikmisg@gmail.com>
2023-03-08 16:11:05 -0600

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
     useEffect(() => {
         dispatch(fetchCampsites());
         dispatch(fetchPartners());
+        dispatch(fetchPromotions());
     }, [dispatch]);
 
     return (
--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -7,7 +7,7 @@ export const fetchPartners = createAsyncThunk(
     async () => {
         const response = await fetch(baseUrl + 'partners');
         if(!response.ok) {
-            return Promise.reject('Unable to fetch, status:', response.status);
+            return Promise.reject('Unable to fetch, status: ', response.status);
         }
         const data = await response.json();
         return data;
